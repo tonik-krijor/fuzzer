f7e9ca65d8ca83698954431b3180356225055cc0
Toni Karppi <karppitoni@gmail.com>
2019-04-20 20:23:02 +0200

    Rewrite pre-file generator

--- create_pre_files.sh
+++ create_pre_files.sh
@@ -1,167 +1,163 @@
 #!/bin/bash
 
-# Creates pre-files for the `cat` command.
-cat_in_dir="args/cat.txt"
-cat_out_dir="pre/cat.txt"
-{
-  mixemup $cat_in_dir --max-args 3 &&
-  mixemup $cat_in_dir --postfix files/f1 --max-args 3;
-} > $cat_out_dir
+# Input files
+cat_in="args/cat.txt"
+cp_in="args/cp.txt"
+date_in="args/date.txt"
+dd_in="args/dd.txt"
+df_in="args/df.txt"
+dir_in="args/dir.txt"
+echo_in="args/echo.txt"
+false_in="args/false.txt"
+ln_in="args/ln.txt"
+ls_in="args/ls.txt"
+mkdir_in="args/mkdir.txt"
+mktemp_in="args/mktemp.txt"
+mv_in="args/mv.txt"
+printf_in="args/printf.txt"
+pwd_in="args/pwd.txt"
+sleep_in="args/sleep.txt"
+touch_in="args/touch.txt"
+true_in="args/true.txt"
+uname_in="args/uname.txt"
+vdir_in="args/vdir.txt"
 
-# Creates pre-files for the `cp` command.
-cp_in_dir="args/cp.txt"
-cp_out_dir="pre/cp.txt"
-{
-  mixemup $cp_in_dir --max-args 3 &&
-  mixemup $cp_in_dir --postfix "files/f1 files/f2" --max-args 3;
-} > $cp_out_dir
+# Output files
+cat_out="pre/cat.txt"
+cp_out="pre/cp.txt"
+date_out="pre/date.txt"
+dd_out="pre/dd.txt"
+df_out="pre/df.txt"
+dir_out="pre/dir.txt"
+echo_out="pre/echo.txt"
+false_out="pre/false.txt"
+ln_out="pre/ln.txt"
+ls_out="pre/ls.txt"
+mkdir_out="pre/mkdir.txt"
+mktemp_out="pre/mktemp.txt"
+mv_out="pre/mv.txt"
+printf_out="pre/printf.txt"
+pwd_out="pre/pwd.txt"
+sleep_out="pre/sleep.txt"
+touch_out="pre/touch.txt"
+true_out="pre/true.txt"
+uname_out="pre/uname.txt"
+vdir_out="pre/vdir.txt"
 
-# Creates pre-files for the `date` command.
-date_in_dir="args/date.txt"
-date_out_dir="pre/date.txt"
+# cat
 {
-  mixemup $date_in_dir --max-args 3;
-} > $date_out_dir
+  mixemup $cat_in --max-args 3 &&
+  mixemup $cat_in --postfix files/f1 --max-args 3;
+} > $cat_out
 
-# Creates pre-files for the `dd` command.
-dd_in_dir="args/dd.txt"
-dd_out_dir="pre/dd.txt"
+# cp
+{
+  mixemup $cp_in --max-args 3 &&
+  mixemup $cp_in --postfix "files/f1 files/f2" --max-args 3;
+} > $cp_out
+
+# date
+{
+  mixemup $date_in --max-args 3;
+} > $date_out
+
+# dd
 { 
-  mixemup $dd_in_dir --max-args 3 --postfix "if=files/f1 of=files/f2";
-} > $dd_out_dir
+  mixemup $dd_in --max-args 3 --postfix "if=files/f1 of=files/f2";
+} > $dd_out
 
-# Creates pre-files for the `df` command.
-df_in_dir="args/df.txt"
-df_out_dir="pre/df.txt"
+# df
 { 
-  mixemup $df_in_dir --max-args 3;
-} > $df_out_dir
+  mixemup $df_in --max-args 3;
+} > $df_out
 
-# Creates pre-files for the `dir` command.
-dir_in_dir="args/dir.txt"
-dir_out_dir="pre/dir.txt"
+# dir
 { 
-  mixemup $dir_in_dir --max-args 3 &&
-  mixemup $dir_in_dir --max-args 3 --postfix files;
-} > $dir_out_dir
+  mixemup $dir_in --max-args 3 &&
+  mixemup $dir_in --max-args 3 --postfix files;
+} > $dir_out
 
-# Creates pre-files for the `echo` command.
-echo_in_dir="args/echo.txt"
-echo_out_dir="pre/echo.txt"
+# echo
 { 
-  mixemup $echo_in_dir --max-args 3 &&
-  mixemup $echo_in_dir --max-args 3 --postfix files;
-} > $echo_out_dir
+  mixemup $echo_in --max-args 3 &&
+  mixemup $echo_in --max-args 3 --postfix files;
+} > $echo_out
 
-# Creates pre-files for the `false` command.
-false_in_dir="args/false.txt"
-false_out_dir="pre/false.txt"
+# false
 { 
-  mixemup $false_in_dir --max-args 3 &&
-  mixemup $false_in_dir --max-args 3 --postfix files;
-} > $false_out_dir
+  mixemup $false_in --max-args 3 &&
+  mixemup $false_in --max-args 3 --postfix files;
+} > $false_out
 
-# Creates pre-files for the `ln` command.
-ln_in_dir="args/ln.txt"
-ln_out_dir="pre/ln.txt"
+# ln
 { 
-  mixemup $ln_in_dir --max-args 3 &&
-  mixemup $ln_in_dir --max-args 3 --postfix "files/f1 files/f2";
-} > $ln_out_dir
+  mixemup $ln_in --max-args 3 &&
+  mixemup $ln_in --max-args 3 --postfix "files/f1 files/f2";
+} > $ln_out
 
-# Creates pre-files for the `ls` command.
-ls_in_dir="args/ls.txt"
-ls_out_dir="pre/ls.txt"
+# ls
 { 
-  mixemup $ls_in_dir --max-args 3 &&
-  mixemup $ls_in_dir --max-args 3 --postfix files;
-} > $ls_out_dir
+  mixemup $ls_in --max-args 3 &&
+  mixemup $ls_in --max-args 3 --postfix files;
+} > $ls_out
 
-# Creates pre-files for the `mkdir` command.
-mkdir_in_dir="args/cat.txt"
-mkdir_out_dir="pre/cat.txt"
+# mkdir
 {
-  mixemup $mkdir_in_dir --max-args 3 &&
-  mixemup $mkdir_in_dir --postfix files/d1 --max-args 3;
-} > $mkdir_out_dir
+  mixemup $mkdir_in --max-args 3 &&
+  mixemup $mkdir_in --postfix files/d1 --max-args 3; 
+} > $mkdir_out
 
-# Creates pre-files for the `mkdir` command.
-mkdir_in_dir="args/mkdir.txt"
-mkdir_out_dir="pre/mkdir.txt"
+# mktemp
 { 
-  mixemup $mkdir_in_dir --max-args 3 &&
-  mixemup $mkdir_in_dir --postfix files/d1 --max-args 3; 
-} > $mkdir_out_dir
+  mixemup $mktemp_in --max-args 3 &&
+  mixemup $mktemp_in --postfix helloXXXXXX --max-args 3;
+} > $mktemp_out
 
-# Creates pre-files for the `mktemp` command.
-mktemp_in_dir="args/mktemp.txt"
-mktemp_out_dir="pre/mktemp.txt"
-{ 
-  mixemup $mktemp_in_dir --max-args 3 &&
-  mixemup $mktemp_in_dir --postfix helloXXXXXX --max-args 3;
-} > $mktemp_out_dir
-
-# Creates pre-files for the `mv` command.
-mv_in_dir="args/mv.txt"
-mv_out_dir="pre/mv.txt"
+# mv
 {
-  mixemup $mv_in_dir --max-args 3 &&
-  mixemup $mv_in_dir --postfix files/d3 --max-args 3;
-} > $mv_out_dir
+  mixemup $mv_in --max-args 3 &&
+  mixemup $mv_in --postfix files/d3 --max-args 3;
+} > $mv_out
 
-# Creates pre-files for the `printf` command.
-printf_in_dir="args/printf.txt"
-printf_out_dir="pre/printf.txt"
+# printf
 {
-  mixemup $printf_in_dir --max-args 3 &&
-  mixemup $printf_in_dir --postfix helloworld --max-args 3;
-} > $printf_out_dir
+  mixemup $printf_in --max-args 3 &&
+  mixemup $printf_in --postfix helloworld --max-args 3;
+} > $printf_out
 
-# Creates pre-files for the `pwd` command.
-pwd_in_dir="args/pwd.txt"
-pwd_out_dir="pre/pwd.txt"
+# pwd
 {
-  mixemup $pwd_in_dir --max-args 3 &&
-  mixemup $pwd_in_dir --postfix helloworld --max-args 3;
-} > $pwd_out_dir
+  mixemup $pwd_in --max-args 3 &&
+  mixemup $pwd_in --postfix helloworld --max-args 3;
+} > $pwd_out
 
-# Creates pre-files for the `sleep` command.
-sleep_in_dir="args/sleep.txt"
-sleep_out_dir="pre/sleep.txt"
+# sleep
 {
-  mixemup $sleep_in_dir --max-args 3 &&
-  mixemup $sleep_in_dir --postfix 0.01 --max-args 3 &&
-  mixemup $sleep_in_dir --postfix 0 --max-args 3;
-} > $sleep_out_dir
+  mixemup $sleep_in --max-args 3 &&
+  mixemup $sleep_in --postfix 0.01 --max-args 3 &&
+  mixemup $sleep_in --postfix 0 --max-args 3;
+} > $sleep_out
 
-# Creates pre-files for the `touch` command.
-touch_in_dir="args/touch.txt"
-touch_out_dir="pre/touch.txt"
+# touch
 {
-  mixemup $touch_in_dir --max-args 3 &&
-  mixemup $touch_in_dir --postfix files/f1 --max-args 3;
-} > $touch_out_dir
+  mixemup $touch_in --max-args 3 &&
+  mixemup $touch_in --postfix files/f1 --max-args 3;
+} > $touch_out
 
-# Creates pre-files for the `true` command.
-true_in_dir="args/true.txt"
-true_out_dir="pre/true.txt"
+# true
 {
-  mixemup $true_in_dir --max-args 3 &&
-  mixemup $true_in_dir --postfix helloworld --max-args 3;
-} > $true_out_dir
+  mixemup $true_in --max-args 3 &&
+  mixemup $true_in --postfix helloworld --max-args 3;
+} > $true_out
 
-# Creates pre-files for the `uname` command.
-uname_in_dir="args/uname.txt"
-uname_out_dir="pre/uname.txt"
+# uname
 {
-  mixemup $uname_in_dir --max-args 3 &&
-  mixemup $uname_in_dir --postfix helloworld --max-args 3;
-} > $uname_out_dir
+  mixemup $uname_in --max-args 3 &&
+  mixemup $uname_in --postfix helloworld --max-args 3;
+} > $uname_out
 
-# Creates pre-files for the `vdir` command.
-vdir_in_dir="args/vdir.txt"
-vdir_out_dir="pre/vdir.txt"
+# vdir
 {
-  mixemup $vdir_in_dir --max-args 3 &&
-  mixemup $vdir_in_dir --postfix helloworld --max-args 3;
-} > $vdir_out_dir
+  mixemup $vdir_in --max-args 3 &&
+  mixemup $vdir_in --postfix helloworld --max-args 3;
+} > $vdir_out

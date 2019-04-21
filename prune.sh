c27176056c0b5f4d5a3ac751e108efd4c179a8f9
Toni Karppi <karppitoni@gmail.com>
2019-04-21 13:15:57 +0200

    Simplify pruner

--- prune.sh
+++ prune.sh
@@ -4,18 +4,18 @@ if (( $# < 2 )); then
     echo "Usage: $0 exec_path exec_name [file]"
 else
     PRG=$2
-    cov_max=`gcov $1 2>/dev/null | grep $PRG -A 1 | grep -Eo '[0-9]+\.[0-9]+'`
-    while read line
-    do
+    cov_max=$(gcov $1 2>/dev/null | grep $PRG -A 1 | grep -Eo '[0-9]{1,3}\.[0-9]{2}')
+
+    while read line; do
         # Prune the results - remove stuff that doesn't increase coverage
-        output=`timeout -t 1 $1 $line 2> /dev/null`
+        timeout -t 1 $1 $line &> /dev/null
         retVal=$?
         if [[ $retVal == 143 ]]; then # Command timed out
+            (>&2 echo "timeout")
             continue
         fi
-        cov=`gcov $1 2>/dev/null | grep $PRG -A 1 | grep -Eo '[0-9]+\.[0-9]+'`
-        tmp=`echo -e "$cov\n$cov_max" | sort -n | tail -n 1`
-        if [ "$cov" = "$tmp" ] && [ "$cov" != "$cov_max" ]; then
+        cov=$(gcov $1 2>/dev/null | grep $PRG -A 1 | grep -Eo '[0-9]{1,3}\.[0-9]{2}')
+        if (( $(echo "$cov > $cov_max" | bc -l) )); then
             cov_max=$cov
             echo $line
             (>&2 echo -ne "\r$cov")

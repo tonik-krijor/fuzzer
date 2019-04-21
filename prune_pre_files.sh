c0723130b9e5b79fb0d373f477c8ec793e112dfc
Toni Karppi <karppitoni@gmail.com>
2019-04-21 03:10:24 +0200

    Setup new files directory

---
+++ prune_pre_files.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+docker build -t fuzz .
+mkdir -p init # The pruned files will be saved in the 'init' directory.
+
+declare -a binaries=("cat" "cp" "date" "dd" "df" "dir" "echo"
+                     "false" "ln" "ls" "mkdir" "mktemp" "mv"
+                     "printf" "pwd" "sleep" "touch" "true" "uname" "vdir")
+
+for binary in "${binaries[@]}"
+do
+  echo "Pruning $binary"
+  docker run fuzz ./prune.sh coreutils/src/$binary $binary pre/$binary.txt > init/$binary.txt
+done
